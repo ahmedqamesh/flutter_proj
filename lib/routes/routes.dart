f167b1989b118be6e7a20e357a62cf0838041fbc
ahmedqamesh <ahmedqamesh@github.com>
2022-01-22 21:00:03 +0100

    Welcome screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:flutter_proj/view/screens/auth/sign_up_screen.dart';
 import 'package:flutter_proj/view/screens/welcome_screen.dart';
+import 'package:flutter_proj/view/screens/auth/login_screen.dart';
 
 class AppRoutes {
 //InitialRoute
@@ -10,9 +12,19 @@ class AppRoutes {
       name: Routes.welcomeScreen,
       page: () => const WelcomeScreen(),
     ),
+    GetPage(
+      name: Routes.loginScreen,
+      page: () => const LoginScreen(),
+    ),
+    GetPage(
+      name: Routes.signUpScreen,
+      page: () => const SignUpScreen(),
+    ),
   ];
 }
 
 class Routes {
   static const welcomeScreen = '/welcomeScreen';
+  static const loginScreen = '/loginScreen';
+  static const signUpScreen = '/signUpScreen';
 }
--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +9,7 @@ class SignUpScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Center(
         child: Text(
-          "Login Screen",
+          "LoginScreen",
           style: TextStyle(fontSize: 30),
         ),
       ),
